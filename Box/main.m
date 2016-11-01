e13b8c3c56613e8e70e96997d81ff1b3aeb88e41
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-01 11:14:42 -0400

    Initial Commit

---
+++ Box/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Box
+//
+//  Created by Hamza Lakhani on 2016-11-01.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
